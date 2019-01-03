152181a249ed06e28fbf9365326cafeb30c5ff8f
Jose Luis <jlcastrillon91@gmail.com>
2019-01-03 10:55:18 -0500

    Make search with like instead of exact search

--- src/infra/support/sequelize_search_attrs.js
+++ src/infra/support/sequelize_search_attrs.js
@@ -1,12 +1,24 @@
+const toLikeFilters = (filter) => {
+  var newFilter = {}
+  Object.keys(filter).forEach((key) => {
+    if (typeof filter[key] === 'number') {
+      newFilter[key] = filter[key]
+    } else {
+      newFilter[key] = { $like: '%' + filter[key] + '%' }
+    }
+  })
+  return newFilter
+}
+
 const toSequelizeSearch = (selectFields, searchParams) => {
   const attrs = {
     attributes: selectFields,
     offset: searchParams.pagination.offset || 0,
     limit: searchParams.pagination.limit || 1000000,
-    order: [[searchParams.order.field || 'createdAt', searchParams.order.type || 'DESC']],
+    order: [[searchParams.order.field || 'createdAt', searchParams.order.type || 'DESC']]
   }
   if (searchParams.filter && searchParams.filter !== {}) {
-    Object.assign(attrs, { where: searchParams.filter })
+    Object.assign(attrs, { where: toLikeFilters(searchParams.filter) })
   }
   return attrs
 }

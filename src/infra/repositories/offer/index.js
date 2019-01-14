2af82549634f1fa4ec112fc1ff81ac7b88b80776
Jose Luis <jlcastrillon91@gmail.com>
2019-01-14 17:53:29 -0500

    Add virtual fields in offers filtering, collectionId and sku

--- src/infra/repositories/offer/index.js
+++ src/infra/repositories/offer/index.js
@@ -3,7 +3,24 @@ const BaseRepository = require('../base_repository')
 const container = require('src/container') // we have to get the DI
 // inject database
 const { database } = container.cradle
+const releasesModel = database.models.releases
 
-const OfferRepository = BaseRepository(database.models.offers, Offer)
+const getOptionsCallback = (searchParams) => {
+  const include = { model: releasesModel, as: 'release', attributes: [ 'id', 'sku', 'collectionId' ] }
+  if (searchParams.filter && searchParams.filter.sku) {
+    Object.assign(include, { where: { sku: searchParams.filter.sku } })
+    delete searchParams.filter.sku
+  }
+  if (searchParams.filter && searchParams.filter.collectionId) {
+    Object.assign(include, { where: { collectionId: searchParams.filter.collectionId } })
+    delete searchParams.filter.collectionId
+  }
+  return {
+    include: [ include ],
+    distinct: true
+  }
+}
+
+const OfferRepository = BaseRepository(database.models.offers, Offer, { getOptionsCallback })
 
 module.exports = OfferRepository

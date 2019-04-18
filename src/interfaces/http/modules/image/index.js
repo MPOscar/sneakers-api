58d4ddc52af23654cd0a192f75773448442e8ac4
Jose Luis <jlcastrillon91@gmail.com>
2019-04-18 10:47:02 -0400

    remove auth from blog editor image upload endpoint

--- src/interfaces/http/modules/image/index.js
+++ src/interfaces/http/modules/image/index.js
@@ -6,10 +6,7 @@ const baseUrl = process.env.IMAGE_URL || 'http://localhost:3000/api/images/'
 module.exports = () => {
   const router = Router()
   const { auth, response: { Success }, upload } = container.cradle
-  router.use(auth.authenticate())
-
   router.post('/editor', upload.file, function (req, res, next) {
-    console.log(req.file)
     const filename = req.file.filename || req.file.key
     const response = {
       status: true,
@@ -21,8 +18,9 @@ module.exports = () => {
     res.status(Status.OK).json(response)
   })
 
+  router.use(auth.authenticate())
+
   router.post('/', upload.image, function (req, res, next) {
-    console.log(req.file)
     const filename = req.file.filename || req.file.key
     res.status(Status.OK).json(Success({ url: (baseUrl + filename) }))
     // res.status(Status.OK).json(Success({ url: req.file.location }))
